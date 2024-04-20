27ec9ca0e076737f9c82b6c9f3e8095cc0d28f1a
p-afayaz <p-afayaz@P-AFAYAZ>
2024-04-20 10:40:32 +0200

    Add project files.

---
+++ InteractiveChat/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InteractiveChat.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public UserController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
